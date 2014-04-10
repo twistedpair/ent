e5fb3b5db8bc87d530488fb058d2361306de0138
James Halliday <mail@substack.net>
2014-04-09 18:39:32 -0700

    split slow tests out into hex.js

--- test/codes.js
+++ test/codes.js
@@ -37,37 +37,6 @@ test('num', function (t) {
     t.end();
 });
 
-test('hex', function (t) {
-    for (var i = 0; i < 32; i++) {
-        var a = String.fromCharCode(i);
-        if (a.match(/\s/)) {
-            t.equal(ent.decode(a), a);
-        }
-        else {
-            var b = '&#x' + i.toString(16) + ';';
-            t.equal(ent.decode(b), a);
-            t.equal(ent.encode(a), '&#' + i + ';');
-        }
-    }
-    
-    for (var i = 127; i < 2000; i++) {
-        var a = String.fromCharCode(i);
-        var b = '&#x' + i.toString(16) + ';';
-        var c = '&#X' + i.toString(16) + ';';
-        
-        t.equal(ent.decode(b), a);
-        t.equal(ent.decode(c), a);
-        
-        var encoded = ent.encode(a);
-        var encoded2 = ent.encode(a + a);
-        if (!encoded.match(/^&\w+;/)) {
-            t.equal(encoded, '&#' + i + ';');
-            t.equal(encoded2, '&#' + i + ';&#' + i + ';');
-        }
-    }
-    t.end();
-});
-
 test('astral num', function (t) {
     var a = punycode.ucs2.encode([0x1d306]);
     var b = '&#x1d306;';
